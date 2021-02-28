51f0c56891343bbcd4934c528f70d7c983a5f140
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-02-28 20:38:00 +0300

    fix: algorytm now works currently with double negate (--)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function calculateExpression(expr) {
+function calculateExpression(expression) {
   // ALWAYS SORT BY PRIORITY
   const OPERATORS = {
     '^': (a, b) => a ** b,
@@ -23,6 +23,7 @@ function calculateExpression(expr) {
   const BRACKETS_REGEXP = new RegExp(/\w*?\(([^()]*)\)/);
 
   function _parse(expression) {
+    console.log('string - ' + expression);
     let deepestExp = _findDeepestExpression(expression);
     // console.log(deepestExp);
     if (deepestExp)
@@ -39,31 +40,33 @@ function calculateExpression(expr) {
     return expression.match(BRACKETS_REGEXP)[1];
   }
 
-  function _calc(expression) {
-    // console.log('first - ' + expression);
+  function _calc(_expression) {
+    // _expression = _expression.replace(/[^\d\/\*\-\+\^]?(--)/, '');
+    _expression = _expression.replace(/([\+\-\/\*])(--)|(^--)/, '$1');
+
+    // console.log('first - ' + _expression);
     for (let operator in OPERATORS) {
       let regExp = `${NUMBER_REGEXP}\\${operator}${NUMBER_REGEXP}`;
-      // console.log('regexp - ' + regExp);
-      let matches;
-      while ((matches = expression.match(regExp))) {
-        let [expr, a, b] = matches;
+      let match;
+      while ((match = _expression.match(regExp))) {
+        let [expr, a, b] = match;
         let result = OPERATORS[operator](+a, +b);
         // console.log(`here - ${result}`);
-        expression = /[\d]/.test(expression[expression.indexOf(expr) - 1])
-          ? expression.replace(expr, `+${result}`)
-          : expression.replace(expr, result);
-        // console.log(expression);
+        _expression = /[\d]/.test(_expression[_expression.indexOf(expr) - 1])
+          ? _expression.replace(expr, `+${result}`)
+          : _expression.replace(expr, result);
+
+        // console.log('expr - ' + _expression);
       }
     }
-    // console.log('last - ' + expression);
-    return +expression;
+    // console.log('last - ' + _expression);
+    return +_expression;
   }
 
-  return _parse(expr.replace(/\s/g, ''));
+  return _parse(expression.replace(/\s/g, ''));
 }
-
-console.log(
-  calculateExpression(
-    ' 24 - 23 * 17 / (  93 + 52 * 70 * (  6 + 91 / (  (  4 / 39 / 8 * 30  ) / (  22 * 97 * (  32 * 20 * (  82 - 80 * 51 / 89 * 9  ) * 56 + 82  ) * 89  ) - 17 - 17  ) / 29 / 81  )  ) '
-  )
-);
+// console.log(
+//   calculateExpression(
+//     ' 24 - 23 * 17 / (  93 + 52 * 70 * (  6 + 91 / (  (  4 / 39 / 8 * 30  ) / (  22 * 97 * (  32 * 20 * (  82 - 80 * 51 / 89 * 9  ) * 56 + 82  ) * 89  ) - 17 - 17  ) / 29 / 81  )  ) '
+//   )
+// );
